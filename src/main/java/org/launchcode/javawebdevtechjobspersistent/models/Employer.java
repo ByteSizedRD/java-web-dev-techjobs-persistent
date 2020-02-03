ea799c1290194c6bdd1eeed0ac6f01fa1b255cfc
ByteSizedRD <56002023+ByteSizedRD@users.noreply.github.com>
2020-02-02 23:19:39 -0600

    Updated Employer and Skill model classes with @Entity, constructor, getters/setters

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    public String location;
 
+
+    public Employer(String location) {
+        this.()
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
