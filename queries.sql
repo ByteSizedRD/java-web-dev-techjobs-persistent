9abdc9c02620680dca0e596f82c0f77cc629eb63
ByteSizedRD <56002023+ByteSizedRD@users.noreply.github.com>
2020-02-06 15:47:03 -0600

    correct queries.sql

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
 ## Part 1: Test it with SQL
 SELECT * FROM techjobs.job;
 
-SHOW COLUMNS FROM techjobsj.job;
+SHOW COLUMNS FROM techjobs.job;
 
-id: int(11), employer: varchar(255), name: varchar(255), skills: varchar(255)
+--id: int(11), employer: varchar(255), name: varchar(255), skills: varchar(255)
 ## Part 2: Test it with SQL
 
 SELECT * FROM employer
@@ -14,3 +14,9 @@ WHERE location='Saint Louis'
 DROP TABLE techjobs.job;
 
 ## Part 4: Test it with SQL
+
+SELECT name, description
+FROM skill
+INNER JOIN job_skills ON job_skills.skills_id = skill.id;
+
+
